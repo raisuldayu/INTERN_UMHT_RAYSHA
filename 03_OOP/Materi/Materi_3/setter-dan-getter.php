706767fa5b0a1c4ea1bf96a078e4421a9deea5a3
Raisa <zulkarnainraysha@gmail.com>
2025-02-18 16:31:54 +0700

    baru

---
+++ 03_OOP/Materi/Materi_3/setter-dan-getter.php
@@ -0,0 +1,124 @@
+<?php 
+
+class Produk {
+    private $judul,
+           $penulis,
+           $penerbit,
+           $harga,
+           $diskon = 0;
+    
+     public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
+        $this->judul = $judul; 
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+     }
+
+     public function setJudul( $judul ) {
+        $this->judul = $judul;
+     }
+
+     public function getJudul() {
+        return $this->judul;
+     }
+
+     public function setPenulis( $penulis ) {
+        $this->penulis = $penulis;
+     }
+
+     public function getPenulis() {
+        return $this->penulis;
+     }
+
+     public function setPenerbit( $penerbit ) {
+        $this->penerbit = $penerbit;
+     }
+
+     public function setDiskon( $diskon ) {
+        $this->diskon = $diskon; 
+     }
+
+     public function getDiskon() {
+        return $this->diskon;
+     }
+
+     public function getPenerbit() {
+        return $this->penerbit;
+     }
+
+     public function setHarga( $harga ) {
+        $this->harga = $harga;
+     }
+     
+     public function getHarga() {
+        return $this->harga - ( $this->harga * $this->diskon / 100 );
+    }
+
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoProduk() {
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+
+        return $str;
+    }
+}
+
+
+class Idol extends Produk {
+    public $jmlHalaman;
+
+    public function __construct(  $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0 ) {
+
+        parent::__construct(  $judul, $penulis, $penerbit, $harga, $jmlHalaman = 0);
+
+        $this->jmlHalaman = $jmlHalaman;
+    }
+    
+    public function getInfoProduk() {
+        $str = "Idol : " . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+
+
+class Novel extends Produk {
+    public $waktuBaca;
+
+    public function __construct(  $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuBaca = 0) {
+        parent::__construct(  $judul, $penulis, $penerbit, $harga, $waktuBaca = 0);
+
+        $this->waktuBaca = $waktuBaca;
+    }
+
+    public function getInfoProduk() {
+        $str = "Novel : " . parent::getInfoProduk() . " - {$this->waktuBaca} Jam.";
+        return $str;
+    }
+}
+
+
+class CetakInfoProduk {
+    public function cetak( Produk $produk ) {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+
+$produk1 = new Idol("Seventeen", "Carat", "PLEDIS ENT", 30000, 100);
+$produk2 = new Novel("Ayat Ayat cinta", "Habiburrahman El Shirazy", "Basmala & Republika", 250000, 50);
+
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
+echo "<hr>";
+
+$produk2->setDiskon(50);
+echo $produk2->getHarga();
+echo "<hr>";
+
+$produk1->setJudul("H2H");
+echo $produk1->getJudul();
+?>
